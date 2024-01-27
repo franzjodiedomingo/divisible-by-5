7ac3f8967d437c54c77c856b000447597d4f0bf8
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-27 10:34:16 +0800

    try

--- divisiblebyfive.py
+++ divisiblebyfive.py
@@ -1,2 +1,8 @@
-# Print numbers from a list that are divisible by 5.
+# Print numbers from a list that are divisible by 5
 # Make a list
+
+def display_divisible_by_5(my_list):
+    divisible_numbers = [num for num in my_list if num % 5 == 0]
+
+my_list = [10, 20, 33, 46, 55]
+display_divisible_by_5 (my_list)
