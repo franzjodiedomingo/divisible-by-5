f883e947056f26b54a40731b48b2d0db54bf359c
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-27 23:03:16 +0800

    final

--- divisiblebyfive.py
+++ divisiblebyfive.py
@@ -3,6 +3,8 @@
 
 def display_divisible_by_5(my_list):
     divisible_numbers = [num for num in my_list if num % 5 == 0]
+    print("Divisible by 5:", divisible_numbers)
 
-my_list = [10, 20, 33, 46, 55]
-display_divisible_by_5 (my_list)
+my_list = 10, 20, 33, 46, 55
+print ("Given list is:", my_list)
+display_divisible_by_5(my_list)
